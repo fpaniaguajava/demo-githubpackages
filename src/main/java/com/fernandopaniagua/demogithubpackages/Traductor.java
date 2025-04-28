e4177ed1ba3e3059cc89705a2bd1469d92e6aff3
lafabricadeandroides <fernando.paniagua.java@gmail.com>
2025-04-28 09:30:33 +0200

    Initial commit

---
+++ src/main/java/com/fernandopaniagua/demogithubpackages/Traductor.java
@@ -0,0 +1,7 @@
+package com.fernandopaniagua.demogithubpackages;
+
+public class Traductor {
+    public static String traducir(String texto) {
+        return String.format("El texto %s traducido", texto);
+    }
+}

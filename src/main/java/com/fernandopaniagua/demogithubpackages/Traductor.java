917c13e2012f35694c1801a0f08c8d88962e02fd
lafabricadeandroides <fernando.paniagua.java@gmail.com>
2025-04-28 11:36:12 +0200

    Update Traductor.java

--- src/main/java/com/fernandopaniagua/demogithubpackages/Traductor.java
+++ src/main/java/com/fernandopaniagua/demogithubpackages/Traductor.java
@@ -2,6 +2,6 @@ package com.fernandopaniagua.demogithubpackages;
 
 public class Traductor {
     public static String traducir(String texto) {
-        return String.format("El texto %s traducido", texto);
+        return String.format("El texto %s traducido en versión 2", texto);
     }
 }
